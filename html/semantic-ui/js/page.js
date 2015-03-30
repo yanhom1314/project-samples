61f1c2e07b23bf42cd0a43e8e06d356a84a3cb0e
yafengli <ya_feng_li@163.com>
2015-03-30 12:48:32 +0800

    ok semantic-ui Pagination.

--- html/semantic-ui/js/page.js
+++ html/semantic-ui/js/page.js
@@ -5,8 +5,7 @@ function Pagination(obj) {
     this.pageNum = 1; //current page number
     this.total = 0; //total count
     this.totalPage = 0; 
-    this.barSize = obj.barSize; //分页工具条上展现的页码数
-    this.numPoint=1;  
+    this.barSize = obj.barSize; //分页工具条上展现的页码数    
     this.data = obj.data;
     this.success = obj.success;
     this.error = obj.error;
@@ -16,15 +15,15 @@ function Pagination(obj) {
 Pagination.prototype.init = function () {
     if (this.data == undefined) {
         this.data = {}
-    }
-    
+    }    
     this.div = $('#' + this.id);
     this.fetchData(1);
 };
  
 Pagination.prototype.fetchData = function (pageNum) {
-    this.data.pageNum = pageNum;
-    this.data.pageSize = this.pageSize;
+    this.pageNum = pageNum;
+    this.pageSize = this.pageSize;
+    
     var that = this;
     $.ajax({
         url: that.url,
@@ -51,13 +50,19 @@ Pagination.prototype.fetchData = function (pageNum) {
 Pagination.prototype.showUI = function () {	
     var that = this;
     this.div.empty();
-    var currentBarSize=this.totalPage-(this.numPoint-1)*this.barSize;
-    currentBarSize = currentBarSize > this.barSize ? this.barSize : currentBarSize;
-    this.div.append('<a class="icon item"><i class="left arrow icon"></i> 上一页</a>');
-    for (var i = 0; i < currentBarSize; i++) {    
-    	this.div.append('<a class="item">' + (this.pageNum+i) + '</a>');
+    var bar = this.barArray();
+    console.log("bar[0]:"+bar[0] + " bar[1]"+bar[1]);
+    //previous 
+    if(this.pageNum <= 1) this.div.append('<span class="disabled icon item"><i class="left arrow icon"></i> 上一页</span>');
+    else this.div.append('<a class="icon item"><i class="left arrow icon"></i> 上一页</a>');
+    //bar    
+    for (var i = bar[0]; i <= bar[1]; i++) {    
+    	if(i == this.pageNum) this.div.append('<a class="active item">' + i + '</a>');
+    	else this.div.append('<a class="item">' + i + '</a>');
     }
-    this.div.append('<a class="icon item"> 下一页 <i class="icon right arrow"></i></a>');
+    //next    
+    if(this.pageNum < this.totalPage) this.div.append('<a class="icon item"> 下一页 <i class="icon right arrow"></i></a>');
+    else this.div.append('<span class="disabled icon item"> 下一页 <i class="icon right arrow"></i></span>');
  
     var array = this.div.find('a');
     for (var j = 0; j < array.length; j++) {
@@ -69,37 +74,25 @@ Pagination.prototype.showUI = function () {
         } else {
             current.click({param: that}, function (event) {
                 var p = event.data.param;
-                var n = $(this).text().trim();                
-                that.pageNum = parseInt(n);
-                console.log("that pageNum:"+that.pageNum);
+                var n = $(this).text().trim();                                
                 p.fetchData(parseInt(n));
             })
         }
     }        
 };
+Pagination.prototype.barArray = function() {
+	var pageFix = Math.ceil(this.barSize / 2);
+	var min = (this.pageNum - pageFix <= 1) ? 1 : this.pageNum - pageFix;
+	var max= (this.pageNum + pageFix > this.totalPage) ? this.totalPage : this.pageNum + pageFix;
+	return [min,max];
+};
 Pagination.prototype.nextPage = function (event) {
     var that = event.data.param;
-    if (that.numPoint > 1) {
-        that.div.find('a').first().attr('class', 'item');
-    }
-    if (that.numPoint >= Math.ceil(that.totalPage/that.barSize)) {
-        that.div.find('a').last().attr('class', 'disabled item');
-    } else {
-        that.pageNum=that.numPoint*that.barSize+1;
-        that.numPoint++;
-        that.showUI();
-    }
+    that.pageNum++;        
+    that.showUI();    
 };
 Pagination.prototype.previewPage = function (event) {
     var that = event.data.param;
-    if (that.numPoint <Math.ceil(that.totalPage/that.barSize)) {
-        that.div.find('a').last().attr('class', 'item');
-    }
-    if (that.numPoint == 1) {
-        that.div.find('a').first().attr('class', 'disabled item');
-    } else {
-        that.numPoint--;
-        that.pageNum=(that.numPoint-1)*that.barSize+1;
-        that.showUI();
-    }
+    that.pageNum--;        
+    that.showUI();   
 };
