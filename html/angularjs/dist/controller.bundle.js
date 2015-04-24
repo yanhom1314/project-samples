cdc1ec9db97c169c26938439e353225dc6940b62
yafengli <ya_feng_li@163.com>
2015-04-24 16:15:44 +0800

    ng-repeat is not ok.

--- html/angularjs/dist/controller.bundle.js
+++ html/angularjs/dist/controller.bundle.js
@@ -48,12 +48,12 @@
 
 	ha.controller("HelloCtrl", ['$scope', '$http', function($scope, $http) {
 		/*
-		$scope.phones = [
-			{name: "Nexus S", snippet: "Fast just got faster with Nexus S."},
-			{name: "Motorola XOOM? with Wi-Fi", snippet: "The Next, Next Generation tablet."},
-			{name: "MOTOROLA XOOM?", snippet: "The Next, Next Generation tablet."}
-		];
-		*/
+		   $scope.phones = [
+		   {name: "Nexus S", snippet: "Fast just got faster with Nexus S."},
+		   {name: "Motorola XOOM? with Wi-Fi", snippet: "The Next, Next Generation tablet."},
+		   {name: "MOTOROLA XOOM?", snippet: "The Next, Next Generation tablet."}
+		   ];
+		   */
 		$http.get("/js/data.json").success(function(data, status, headers, config){
 			console.log(data);
 			$scope.phones = data;
@@ -65,42 +65,35 @@
 
 	//Hello
 	ha.controller("Hello", ['$scope', '$http', function ($scope, $http) {
-	    $scope.message = {loading: false};
-	    $scope.show = function (info) {
-	        $scope.message.loading = true;
-	        setTimeout(function () {
-	            $http.get('/js/form.json' + info.key).success(function (data) {
-	                $scope.info = data;
-	                $scope.message = {loading: false};
-	            });
-	        }, 2000);
-	    };
+		$scope.message = {loading: false};
+		$scope.show = function (info) {
+			$scope.message.loading = true;
+			setTimeout(function () {
+				$http.get('/js/form.json' + info.key).success(function (data) {
+					$scope.info = data;
+					$scope.message = {loading: false};
+				});
+			}, 2000);
+		};
 
-	    $scope.reset = function () {
-	        $scope.info = angular.copy({});
-	    };
+		$scope.reset = function () {
+			$scope.info = angular.copy({});
+		};
 
-	    //init
-	    $scope.saveSession = function () {
-	        if ($scope._form.$valid) {
-	            alert("Saving");
-	        }
-	    };
-	    $scope.reset();
+		//init
+		$scope.saveSession = function () {
+			if ($scope._form.$valid) {
+				alert("Saving");
+			}
+		};
+		$scope.reset();
 	}]);
 
 	//Sync
 	ha.controller("Sync", ['$scope', '$http', function ($scope, $http) {
-	    $scope.message = {loading: false};
-	    $scope.show = function (info) {
-	        $scope.message.loading = true;
-	        setTimeout(function () {
-	            $http.get('/js/content.json' + info.key).success(function (data) {
-	                $scope.data = data;
-	                $scope.message = {loading: false};
-	            });
-	        }, 2000);
-	    };
+		$http.get('/js/content.json').success(function (data) {
+			$scope.data = data;
+		});
 	}]);
 
 
--- html/angularjs/js/controller.js
+++ html/angularjs/js/controller.js
@@ -2,12 +2,12 @@ var ha = angular.module("helloApp", []);
 
 ha.controller("HelloCtrl", ['$scope', '$http', function($scope, $http) {
 	/*
-	$scope.phones = [
-		{name: "Nexus S", snippet: "Fast just got faster with Nexus S."},
-		{name: "Motorola XOOM? with Wi-Fi", snippet: "The Next, Next Generation tablet."},
-		{name: "MOTOROLA XOOM?", snippet: "The Next, Next Generation tablet."}
-	];
-	*/
+	   $scope.phones = [
+	   {name: "Nexus S", snippet: "Fast just got faster with Nexus S."},
+	   {name: "Motorola XOOM? with Wi-Fi", snippet: "The Next, Next Generation tablet."},
+	   {name: "MOTOROLA XOOM?", snippet: "The Next, Next Generation tablet."}
+	   ];
+	   */
 	$http.get("/js/data.json").success(function(data, status, headers, config){
 		console.log(data);
 		$scope.phones = data;
@@ -19,40 +19,33 @@ ha.controller("SayHello", ['$scope', '$http', function($scope, $http){
 
 //Hello
 ha.controller("Hello", ['$scope', '$http', function ($scope, $http) {
-    $scope.message = {loading: false};
-    $scope.show = function (info) {
-        $scope.message.loading = true;
-        setTimeout(function () {
-            $http.get('/js/form.json' + info.key).success(function (data) {
-                $scope.info = data;
-                $scope.message = {loading: false};
-            });
-        }, 2000);
-    };
+	$scope.message = {loading: false};
+	$scope.show = function (info) {
+		$scope.message.loading = true;
+		setTimeout(function () {
+			$http.get('/js/form.json' + info.key).success(function (data) {
+				$scope.info = data;
+				$scope.message = {loading: false};
+			});
+		}, 2000);
+	};
 
-    $scope.reset = function () {
-        $scope.info = angular.copy({});
-    };
+	$scope.reset = function () {
+		$scope.info = angular.copy({});
+	};
 
-    //init
-    $scope.saveSession = function () {
-        if ($scope._form.$valid) {
-            alert("Saving");
-        }
-    };
-    $scope.reset();
+	//init
+	$scope.saveSession = function () {
+		if ($scope._form.$valid) {
+			alert("Saving");
+		}
+	};
+	$scope.reset();
 }]);
 
 //Sync
 ha.controller("Sync", ['$scope', '$http', function ($scope, $http) {
-    $scope.message = {loading: false};
-    $scope.show = function (info) {
-        $scope.message.loading = true;
-        setTimeout(function () {
-            $http.get('/js/content.json' + info.key).success(function (data) {
-                $scope.data = data;
-                $scope.message = {loading: false};
-            });
-        }, 2000);
-    };
+	$http.get('/js/content.json').success(function (data) {
+		$scope.data = data;
+	});
 }]);
