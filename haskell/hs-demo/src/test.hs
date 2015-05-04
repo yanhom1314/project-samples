14439ba746cf8c949bfef5404c867a452968578a
yafengli <ya_feng_li@163.com>
2015-05-04 11:48:41 +0800

    add native2ascii page.

--- haskell/hs-demo/src/test.hs
+++ haskell/hs-demo/src/test.hs
@@ -1,11 +1,18 @@
 -- 测试内容
 module Test where
 
+
+doubleMe x = x * 2
+doubleUs x y = x*2 + y*2
+doubleSn x = if x > 10
+			then x * 2
+			else x
+			
 fib x
 	| x < 2 = x
 	| otherwise = fib (x - 1) + fib (x - 2)
 
-main = do 
+main = do
     putStrLn "Greetings!  What is your name?"
     inpStr <- getLine
-    putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"    
+    putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"
---
+++ html/angularjs/js/native2ascii.js
@@ -0,0 +1,26 @@
+var _n2a = angular.module('n2aApp',[]).factory('time',function($timeout){
+    var time={};
+    (function tick(){
+        time.now=new Date().toString();
+        $timeout(tick,1000);
+    })();
+    return time;
+});
+
+_n2a.controller("TranslateCtrl", ['$scope', '$http', 'time', function ($scope, $http, time) {
+    $scope.time=time;
+
+    $scope.native2ascii= function (data) {
+        var regexp = /[^\x00-\xff]/g;
+        var a = data.native;
+        var m = null;
+        while (m = regexp.exec(data.native)) {
+            a = a.split(m[0]).join(escape(m[0]).split("%").join("\\"));
+        }
+        data.ascii= a.toLowerCase();
+    };
+
+    $scope.ascii2native = function (data) {
+        data.native = unescape(data.ascii.split("\\").join("%")).toLowerCase();
+    };
+}]);
