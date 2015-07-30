4811fdea1a34cb67f6493995cbf8da964027420a
yafengli <ya_feng_li@163.com>
2015-07-30 15:43:07 +0800

    golang.

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"mathii"
 	"newmath"
+	"os"
 )
 
 func main() {
@@ -11,4 +14,18 @@ func main() {
 	fmt.Printf("Hello World!\n")
 	fmt.Printf("Number:%d\n", result)
 	mathii.SayHi("YaFengLi")
+	fi, err := os.Open("e:/tmp/1.txt")
+
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	reader := bufio.NewReader(fi)
+	for {
+		n, err := reader.ReadString()
+	}
 }
