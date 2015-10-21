c18696c308720e9540b0000488a72101b0198ec2
ya_feng_li <ya_feng_li@163.com>
2015-10-21 16:50:37 +0800

    demo cjson.

---
+++ c_c++/demo_pcap/include/hello.h
@@ -0,0 +1,12 @@
+//
+// Created by YaFengLi on 2015/10/21.
+//
+
+#ifndef DEMO_HELLO_H
+#define DEMO_HELLO_H
+
+#include <stdio.h>
+
+int add(int x, int y);
+
+#endif //DEMO_HELLO_H
---
+++ c_c++/demo_pcap/src/hello.cc
@@ -0,0 +1,13 @@
+//
+// Created by YaFengLi on 2015/10/21.
+//
+#include "hello.h"
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
+
+int add(int x, int y) {
+    return x + y;
+}
