b2ce98ee8f9e97a479f70b294bada8d72d5618ca
ya_feng_li@163.com <ya_feng_li@163.com>
2016-04-27 21:25:26 +0800

    ok demo akka http.

--- scala/demo-akka/client/src/main/scala/demo/HttpApp.scala
+++ scala/demo-akka/client/src/main/scala/demo/HttpApp.scala
@@ -2,7 +2,7 @@ package demo
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.xml.ScalaXmlSupport._
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 
@@ -18,23 +18,46 @@ object HttpApp extends App {
   val port = 80
 
   val route =
-    path("hello") {
-      get {
-        complete {
-          <h1>Say hello to akka-http</h1>
+    get {
+      pathPrefix("hello" / Segment) { name =>
+        path(Segment) { age =>
+          complete {
+            HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`, s"""<h1>Hello, Mr.$name age is $age !!!</h1>"""))
+          }
         }
       }
     }
 
   val r1 =
-    path("index") {
-      get {
-        complete {
-          <h2>What the fucking hell!</h2>
+    get {
+      path("index") {
+        parameter('color ? "red") { color =>
+          parameter('name ? "NB") { name =>
+            complete(s"The color is '$color' and the background is $name")
+          }
         }
       }
     }
-  val bindingFuture = Http().bindAndHandle(route ~ r1, host, port)
+
+  val r2  =
+     get {
+      path("say") {
+        parameters("color" ? "red", 'name ? "NB") { (color, name) =>
+          complete(s"Say!!! The color is '$color' and the background is $name")
+        }
+      }
+    }
+
+  val r3 =
+    get  {
+      path("hi" / Segment / Segment) { (name,age) =>
+        complete {
+          HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`,s"""<h2>Nice Hello,Mr. $name age is $age !!!!!!</h2>"""))
+        }
+      }
+    }
+
+  val bindingFuture = Http().bindAndHandle(route ~ r1 ~ r2 ~ r3 , host, port)
 
   println(s"Server online at http://${host}:$port/\nPress [q]uit|[e]xit to stop...")
 
