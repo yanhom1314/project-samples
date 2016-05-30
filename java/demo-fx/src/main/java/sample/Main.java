dc5ccb3d5ff0c9b1e83c690c5990c973dee17df5
ya_feng_li <ya_feng_li@163.com>
2016-05-30 15:30:37 +0800

    javafx demo.

--- java/demo-fx/src/main/java/sample/Main.java
+++ java/demo-fx/src/main/java/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-
+        primaryStage.getIcons().add(new Image(this.getClass().getClassLoader().getResourceAsStream("icon.jpg")));
         Scene scene = new Scene(root, 280, 192);
         primaryStage.setScene(scene);
         scene.getStylesheets().add(this.getClass().getClassLoader().getResource("login.css").toExternalForm());
