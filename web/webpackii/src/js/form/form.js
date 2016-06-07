afa3d92694be7b6e32e7e4558e13296b293edf75
ya_feng_li <ya_feng_li@163.com>
2016-06-07 10:21:02 +0800

    vue+jquery+validation+webpack.

--- web/webpackii/src/js/form/form.js
+++ web/webpackii/src/js/form/form.js
@@ -2,21 +2,32 @@ var $ = require("jquery");
 require("jquery-validation");
 
 // jQuery.validator增加IP验证
-$.validator.addMethod("ip", function (value, element) {
+$.validator.addMethod("c_ip", function (value, element) {
     return this.optional(element) || (/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/.test(value) && (RegExp.$1 < 256 && RegExp.$2 < 256 && RegExp.$3 < 256 && RegExp.$4 < 256));
-}, "Please enter a valid ip address.");
+}, "请输入正确IP地址!");
 
 /* 采用不严格的方式验证电话号码。只要电话号码由数字或者数字加"-"(最多两个"-")组 *合构成，均符合。比如：400-823-823， 95555, 010-81567415, 18857107619
  * */
-$.validator.addMethod("lenientTel", function (value, element) {
+$.validator.addMethod("c_phone", function (value, element) {
     return this.optional(element) || (/^([0-9]{1,9}(\-)?)?([0-9]{1,9}){1}(\-[0-9]{1,9})?$/.test(value));
 }, "电话号码格式错误!");
 
+$.validator.addClassRules("ip",{required:true, c_ip:true});
+$.validator.addClassRules("phone",{required:true, c_phone:true});
+
+
+// alias required to cRequired with new message
+ $.validator.addMethod("cRequired", $.validator.methods.required,
+   "Customer name required");
+ // alias minlength, too
+ $.validator.addMethod("cMinlength", $.validator.methods.minlength,
+   // leverage parameter replacement for minlength, {0} gets replaced with 2
+   $.validator.format("Customer name must have at least {0} characters"));
+ // combine them both, including the parameter for minlength
+ $.validator.addClassRules("customer", { cRequired: true, cMinlength: 2 });
+
 module.exports = {
     check: function (id) {
-        $.validator.addMethod("lenientTel", function (value, element) {
-            return this.optional(element) || (/^([0-9]{1,9}(\-)?)?([0-9]{1,9}){1}(\-[0-9]{1,9})?$/.test(value));
-        }, "电话号码格式错误!");
         $("#" + id).validate();
     }
 };
--- web/webpackii/src/js/page/index.js
+++ web/webpackii/src/js/page/index.js
@@ -1,5 +1,6 @@
 require('../../css/main.css'); //加载初始化样式
 var $ = require('jquery')
+require("jquery-validation");
 var add = require('math').add;
 var check = require('form').check;
 
