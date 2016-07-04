b23b95987f13142eb9dc90a2da7d107ead44df5a
ya_feng_li <ya_feng_li@163.com>
2016-07-04 13:16:27 +0800

    fx2048 is working.

--- java/fx2048/src/main/java/g2048/GameController.java
+++ java/fx2048/src/main/java/g2048/GameController.java
@@ -2,11 +2,18 @@ package g2048;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
-public class GameController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static g2048.GameVals.gameManager;
+
+public class GameController implements Initializable {
     @FXML
-    private GamePaneII gamePane;
+    private StackPane gamePane; //private GamePane gamePane;
     @FXML
     private Button mSave;
     @FXML
@@ -20,37 +27,42 @@ public class GameController {
     @FXML
     private Button mQuit;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("gamePane:" + gamePane);
+        System.out.println("mSave:" + mSave);
+        System.out.println("gameManager:" + gameManager);
+    }
+
     @FXML
     public void handleSaveButtonAction(ActionEvent e) {
-        System.out.println("gamePane:" + gamePane);
-        System.out.println("gameManager:" + gamePane.getGameManager());
-        System.out.println("e:" + e.getEventType());
 
-        gamePane.getGameManager().saveSession();
+        System.out.println("e:" + e.getEventType());
+        gameManager.saveSession();
     }
 
     @FXML
     public void handleRestoreButtonAction(ActionEvent e) {
-        gamePane.getGameManager().restoreSession();
+        gameManager.restoreSession();
     }
 
     @FXML
     public void handlePauseButtonAction(ActionEvent e) {
-        gamePane.getGameManager().pauseGame();
+        gameManager.pauseGame();
     }
 
     @FXML
     public void handleReplayButtonAction(ActionEvent e) {
-        gamePane.getGameManager().tryAgain();
+        gameManager.tryAgain();
     }
 
     @FXML
     public void handleInfoButtonAction(ActionEvent e) {
-        gamePane.getGameManager().aboutGame();
+        gameManager.aboutGame();
     }
 
     @FXML
     public void handleQuitButtonAction(ActionEvent e) {
-        gamePane.getGameManager().quitGame();
+        gameManager.quitGame();
     }
 }
