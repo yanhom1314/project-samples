57e30344a590260102d0b41d644b985693f040c8
ya_feng_li <ya_feng_li@163.com>
2016-09-28 15:19:12 +0800

    go demo json.

--- go/src/demo/main.go
+++ go/src/demo/main.go
@@ -1,31 +1,31 @@
-
 package main
 
 import (
-    "fmt"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
 )
 
-type user struct {
-    Un string `json:"un"`
-    Count int   `json:"count"`
-    Ips []IpAddr `json:"ips"`
+type User struct {
+	Un    string   `json:"un"`
+	Count int      `json:"count"`
+	Ips   []IPAddr `json:"ips"`
 }
 
-type IpAddr struct {
-    IpAddr string `json:"ip"`
-    Time string   `json:"time"`
-    Online bool   `json:"online"`
+type IPAddr struct {
+	IP     string `json:"ip"`
+	Time   string `json:"time"`
+	Online bool   `json:"online"`
 }
+
 func main() {
-    un := &user{Un: "test_caocheng_1",Count:100}
-    un.Ips = append(un.Ips, IpAddr{IpAddr: "127.0.0.1", Time: "2016090801220"})
-    un.Ips = append(un.Ips, IpAddr{IpAddr: "127.0.0.2", Time: "2016090801222"})
-    un.Ips = append(un.Ips, IpAddr{IpAddr: "127.0.0.3", Time: "2016090801223",Online:true})
-    b, err := json.Marshal(un)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    fmt.Println(string(b))
+	un := &User{Un: "test_caocheng_1", Count: 100}
+	un.Ips = append(un.Ips, IPAddr{IP: "127.0.0.1", Time: "2016090801220"})
+	un.Ips = append(un.Ips, IPAddr{IP: "127.0.0.2", Time: "2016090801222"})
+	un.Ips = append(un.Ips, IPAddr{IP: "127.0.0.3", Time: "2016090801223", Online: true})
+	b, err := json.Marshal(un)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(b))
 }
