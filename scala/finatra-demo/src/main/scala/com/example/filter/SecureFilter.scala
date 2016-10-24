13c26dba77ceddbc014292a88c5dadbf09c9f806
ya_feng_li <ya_feng_li@163.com>
2016-10-24 21:29:03 +0800

    cookie.

--- scala/finatra-demo/src/main/scala/com/example/filter/SecureFilter.scala
+++ scala/finatra-demo/src/main/scala/com/example/filter/SecureFilter.scala
@@ -1,6 +1,6 @@
 package com.example.filter
 
-import com.twitter.finagle.http.{Request, Response, Status}
+import com.twitter.finagle.http.{Cookie, Request, Response, Status}
 import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.util.Future
 
@@ -11,8 +11,8 @@ class SecureFilter extends SimpleFilter[Request, Response] {
   override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
 
     if (!request.params.contains("name")) {
+      request.response.status_=(Status.Forbidden)
       Future {
-        request.response.status_=(Status.Forbidden)
         request.response
       }
     }
@@ -22,14 +22,12 @@ class SecureFilter extends SimpleFilter[Request, Response] {
     }
   }
 
-  private def trace(request: Request): Future[Unit] = {
-    Future {
-      println(s"#########Query Parameters:${request.params.size}########")
-      request.params.foreach { t => println(s"${t._1}:${t._2}") }
-      println("######################")
-      println(s"#########Cookies:${request.cookies.size}########")
-      request.cookies.foreach { t => println(s"${t._1}:${t._2.value}") }
-      println("######################")
-    }
+  private def trace(request: Request): Unit = {
+    println(s"#########Query Parameters:${request.params.size}########")
+    request.params.foreach { t => println(s"${t._1}:${t._2}") }
+    println("######################")
+    println(s"#########Cookies:${request.cookies.size}########")
+    request.cookies.foreach { t => println(s"${t._1}:${t._2.value}") }
+    println("######################")
   }
 }
