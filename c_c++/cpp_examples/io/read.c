f9de75e3998835235ee92c977eeec04ae05de842
ya_feng_li@163.com <ya_feng_li@163.com>
2016-11-06 17:05:32 +0800

    c_cpp.

--- c_c++/cpp_examples/io/read.c
+++ c_c++/cpp_examples/io/read.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-	FILE * fp;
-	char * line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	fp = fopen("/tmp/files.list", "r");
-	if (fp == NULL){
-		exit(EXIT_FAILURE);
-	}
-	while ((read = getline(&line, &len, fp)) != -1)		{
-			printf("Retrieved line of length %zu :\n", read);
-			printf("%s", line);
-	}
-	if(line){
-		free(line);
-	}
-	return EXIT_SUCCESS;
+
+#define  MAX_LENGTH 1024
+
+void read_write(char *in_file, char *out_file) {
+    char buffer[MAX_LENGTH];
+    int num = 0;
+    FILE *fp1 = fopen(in_file, "r");//打开输入文件
+    FILE *fp2 = fopen(out_file, "w");//打开输出文件
+    if (fp1 == NULL || fp2 == NULL) {//若打开文件失败则退出
+        perror("不能打开文件！");
+    }
+    while (fgets(buffer, MAX_LENGTH, fp1) != NULL) {
+        printf("No.%d.%s\n", num, buffer);
+        fputs(buffer, fp2);
+        num++;
+    }
+
+    fclose(fp1);//关闭输入文件
+    fclose(fp2);//关闭输出文件，相当于保存
+}
+
+int main(void) {
+    read_write("F:\\Projects\\demo\\hello-cpp\\main.cpp","F:\\Projects\\demo\\hello-cpp\\main_out.cpp");
+    return EXIT_SUCCESS;
 }
 
--- c_c++/cpp_examples/md5/md5.c
+++ c_c++/cpp_examples/md5/md5.c
@@ -2,35 +2,38 @@
 #include <openssl/md5.h>
 #include <string.h>
 
-void md5_1()
-{
+void md5_1(unsigned char *data) {
     MD5_CTX ctx;
-    unsigned char *data="123";
     unsigned char md[16];
-    char buf[33]={'\0'};
-    char tmp[3]={'\0'};
+    char buf[33] = {'\0'};
+    char tmp[3] = {'\0'};
     int i;
 
     MD5_Init(&ctx);
-    MD5_Update(&ctx,data,strlen(data));
-    MD5_Final(md,&ctx);
+    MD5_Update(&ctx, data, strlen(data));
+    MD5_Final(md, &ctx);
 
-    for( i=0; i<16; i++ ){
-        sprintf(tmp,"%02X",md[i]);
-        strcat(buf,tmp);
+    for (i = 0; i < 16; i++) {
+        sprintf(tmp, "%02X", md[i]);
+        strcat(buf, tmp);
     }
-    printf("%s\n",buf);    
+    printf("%s\n", buf);
 }
 
-void md5_2(){
-        unsigned char* data =(unsigned char*) "123";
-        unsigned char md[16];
-        int i;
-        char tmp[3]={'\0'},buf[33]={'\0'};
-        MD5((const unsigned char*)data,strlen((const char*)data),md);
-        for (i = 0; i < 16; i++){
-                sprintf(tmp,"%2.2x",md[i]);
-                strcat(buf,tmp);
-        }
-        printf("%s\n",buf);
+void md5_2(unsigned char *data) {
+    unsigned char md[16];
+    int i;
+    char tmp[3] = {'\0'}, buf[33] = {'\0'};
+    MD5((const unsigned char *) data, strlen((const char *) data), md);
+    for (i = 0; i < 16; i++) {
+        sprintf(tmp, "%2.2x", md[i]);
+        strcat(buf, tmp);
+    }
+    printf("%s\n", buf);
+}
+
+int main(int argc, char *argv[]) {
+    md5_1("123");
+    md5_2("123");
+    return 0;
 }
