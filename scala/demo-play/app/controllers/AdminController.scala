a0856506e6e72b73c1e9af87f4f37a10ab0c363e
ya_feng_li <ya_feng_li@163.com>
2016-12-01 16:18:30 +0800

    is working.

--- scala/demo-play/app/controllers/AdminController.scala
+++ scala/demo-play/app/controllers/AdminController.scala
@@ -6,7 +6,7 @@ import org.apache.shiro.realm.Realm
 import play.api.i18n.MessagesApi
 
 class AdminController @Inject()(realm: Realm, val messagesApi: MessagesApi) extends AdminSecured {
-  def sayHi(name: String) = IsAuthenticated {
-    implicit request => Ok(s"<h1>Hello Secure ${name}!</h1>")
+  def admin(name: String = "Anonymous") = IsAuthenticated {
+    Ok(s"<h1>Hello Secure ${name}!</h1>")
   }
 }
