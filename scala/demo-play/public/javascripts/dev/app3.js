0506928526c1152932375062446241134e4a431d
ya_feng_li <ya_feng_li@163.com>
2016-12-22 17:32:04 +0800

    ajax json post and json response.

--- scala/demo-play/public/javascripts/dev/app3.js
+++ scala/demo-play/public/javascripts/dev/app3.js
@@ -6,10 +6,26 @@ require("main-css");
 var add = require('math').add;
 
 $(function () {
+    $.fn.serializeObject = function () {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
     //alert("Hello World!:1+1=" + add(1, 1));
     $("#bt_1").click(function () {
         var f1 = $("#form_1");
-        //alert(f2.attr("action"));
+
+        alert(JSON.stringify(f1.serializeObject()));
         $.ajax({
             url: "/demo/json",
             type: "POST",
@@ -17,7 +33,7 @@ $(function () {
             contentType: 'application/json',
             //data: JSON.stringify(f1.serializeArray()),
             //data: JSON.stringify({un: "admin"}),
-            data: JSON.stringify(f1.serialize()),
+            data: JSON.stringify(f1.serializeObject()),
             success: function (data) {
                 alert(data);
             }
@@ -28,7 +44,7 @@ $(function () {
         //alert(f2.attr("action"));
         $.ajax({
             url: "/demo/check3",
-            data: JSON.stringify(f2.serializeArray()),
+            data: JSON.stringify(f2.serializeObject()),
             type: "POST",
             dataType: "JSON",
             contentType: 'application/json',
