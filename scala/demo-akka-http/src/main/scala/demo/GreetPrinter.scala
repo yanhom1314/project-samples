509820150c9f818b2da9fed09081164e30d362bd
ya_feng_li <ya_feng_li@163.com>
2017-01-04 14:21:56 +0800

    OK

--- scala/demo-akka-http/src/main/scala/demo/GreetPrinter.scala
+++ scala/demo-akka-http/src/main/scala/demo/GreetPrinter.scala
@@ -5,7 +5,6 @@ import akka.actor.Actor
 /**
   * Created by YaFengLi on 2016/12/6.
   */
-// prints a greeting
 class GreetPrinter extends Actor {
   def receive = {
     case Greeting(message) => println(message)
