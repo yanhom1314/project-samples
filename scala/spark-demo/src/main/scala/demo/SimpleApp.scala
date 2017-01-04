6f753b7ef4fc199378f8ddcdf8f7dea2d62978f8
ya_feng_li <ya_feng_li@163.com>
2017-01-04 17:46:24 +0800

    add spark install cluster.

--- scala/spark-demo/src/main/scala/demo/SimpleApp.scala
+++ scala/spark-demo/src/main/scala/demo/SimpleApp.scala
@@ -4,7 +4,7 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object SimpleApp extends App {
 
-  val split_str = "\\|"
+  final val split_str = """|"""
 
   implicit object ArrayString extends ArrayStringOrdering
 
@@ -18,16 +18,20 @@ object SimpleApp extends App {
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
 
-    println("Lines with all:%s  Lines with a: %s, Lines with b: %s".format(numLs, numAs, numBs))
+    println("##########>> Lines with all:%s  Lines with a: %s, Lines with b: %s logData:%s".format(numLs, numAs, numBs, logData.isEmpty()))
 
-    logData.filter(_.split(split_str).length == 2).map { t => val s = t.split("\\|"); Array(s(0).trim.toLowerCase, s(1).trim) }.top(10).foreach {
-      case Array(d, ip) if d.trim.length > 1 && ip.trim.length > 1 => println(s"domain:$d ip:$ip")
-      case e => println(s"ERROR:$e")
+    val m = logData.filter { t => val a = t.split(split_str); a != null && a.length == 2 }
+    if (!m.isEmpty()) {
+      m.map { t => val s = t.split("\\|"); Array(s(0).trim.toLowerCase, s(1).trim) }.top(10).foreach {
+        case Array(d, ip) if d.trim.length > 1 && ip.trim.length > 1 => println(s"domain:$d ip:$ip")
+        case e => println(s"ERROR:$e")
+      }
     }
   }
-  else sys.error(s"Usage:./spark-submit --class demo.SimpleApp x.y.z.jar [/path/file]")
+  else sys.error(s"Usage:./spark-submit --master spark://172.16.9.43:7707 --class demo.SimpleApp x.y.z.jar [/path/file]")
 }
 
 trait ArrayStringOrdering extends Ordering[Array[String]] {
   override def compare(x: Array[String], y: Array[String]): Int = x(1).compareTo(y(1))
 }
+
