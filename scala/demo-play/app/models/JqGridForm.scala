59e973d20f017f95d87f879953a3a43209cb60d6
ya_feng_li <ya_feng_li@163.com>
2017-01-13 15:17:41 +0800

    demo play is ok.

---
+++ scala/demo-play/app/models/JqGridForm.scala
@@ -0,0 +1,3 @@
+package models
+
+case class JqGridForm(var _search: Boolean, val nd: Long, val rows: Int, val page: Int, val sidx: String, val sord: String, val searchField: Option[String], val searchString: Option[String], val searchOper: Option[String], val filters: Option[String])
