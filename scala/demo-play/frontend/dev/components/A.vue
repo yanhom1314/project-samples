f89fc3097c75027eb25cacea35f8f2fb6bf460cc
ya_feng_li <ya_feng_li@163.com>
2017-01-23 17:12:54 +0800

    vue component is ok.

--- scala/demo-play/frontend/dev/components/A.vue
+++ scala/demo-play/frontend/dev/components/A.vue
@@ -1,24 +1,26 @@
 <style scoped>
-.container {
-  border: 1px solid #00f;
-}
-.red {
-  color: #f00;
-}
+    .container {
+        border: 1px solid #00f;
+    }
+
+    .red {
+        color: #f00;
+    }
 </style>
 
 <template>
-  <div class="container">
-    <h2 class="red">{{msg}}</h2>
-  </div>
+    <div class="container">
+        <h2 class="red">{{msg}}</h2>
+    </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: "Hello world!"
+    import getMessage from "../services/messages";
+    export default {
+        data () {
+            return {
+                msg: getMessage()
+            }
+        }
     }
-  }
-}
 </script>
--- scala/demo-play/frontend/dev/loader.js
+++ scala/demo-play/frontend/dev/loader.js
@@ -5,4 +5,5 @@ import App from './App.vue'
 new Vue({
     el: '#app_1',
     render: h => h(App)
-})
+    //也可以使用components:{"node-name":"component-name"} html:<node-name></node-name>
+});
