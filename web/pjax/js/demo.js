6e71bfe48bc26b59217e8437ae7d57e28dd64931
ya_feng_li <ya_feng_li@163.com>
2017-03-16 11:34:17 +0800

    it's working.

--- web/pjax/js/demo.js
+++ web/pjax/js/demo.js
@@ -1,9 +1,20 @@
 var $ = jQuery = require("jQuery");
 require("jquery-pjax");
 
+function active(menu) {
+  menu.addClass('menu-active');
+  menu.attr("disabled","disabled"); 
+}
+
+function on(){
+  
+}
+
 $(function () {  
     $(document).pjax('a', '#container');
     $('#clickMe').click(function () {
+      $(this).addClass('menu-active');
+      $(this).attr("disabled","disabled"); 
       $.pjax({
         url: '3.html',
         container: '#container'
