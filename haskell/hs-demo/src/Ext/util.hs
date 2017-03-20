3f09abd841b9b3ef35c03fe81ace7e2955013afb
ya_feng_li <ya_feng_li@163.com>
2017-03-20 09:29:44 +0800

    clean.

--- haskell/hs-demo/src/Ext/util.hs
+++
@@ -1,7 +0,0 @@
-module Ext.Util (reverseWords,boomBangs) where
-
-reverseWords :: String -> String
-reverseWords = unwords . map reverse . words
-
-boomBangs:: [Integer] -> [String]
-boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
