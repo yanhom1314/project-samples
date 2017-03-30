9a2c1464c398b64d38a0202608c9247cdb1f3a84
ya_feng_li <ya_feng_li@163.com>
2017-03-30 16:22:09 +0800

    delete.

--- scala/demo-play/frontend/dev/loader.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'Vue'
-import App from './App.vue'
-
-/* eslint-disable no-new */
-new Vue({
-    el: '#app_1',
-    render: h => h(App)
-    //也可以使用components:{"node-name":"component-name"} html:<node-name></node-name>
-});
