bbc6e9c8295c5883b898c28657dbe510401dbe48
ya_feng_li <ya_feng_li@163.com>
2017-05-10 08:36:49 +0800

    golang.

--- go/src/modanywhere/server.go
+++ go/src/modanywhere/server.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	 "flag"
- )
+)
+
 func main() {
 	realPath := flag.String("path", "e:/Github/project-samples/web/angularjs", "static resource path")
 	flag.Parse()
-	fmt.Printf("Path:%s.\n",*realPath)
-    http.Handle("/", http.FileServer(http.Dir(*realPath)))
-    http.ListenAndServe(":80", nil)
- }
+	fmt.Printf("Path:%s.\n", *realPath)
+	http.Handle("/", http.FileServer(http.Dir(*realPath)))
+	http.ListenAndServe(":80", nil)
+}
