72e6162602ae99cea9a93c1bf7a2c63f621cd454
ya_feng_li <ya_feng_li@163.com>
2017-07-14 17:20:28 +0800

    ip process.

--- java/sqlbuilder/src/main/java/com/greatbit/ip/IpUtil.java
+++ java/sqlbuilder/src/main/java/com/greatbit/ip/IpUtil.java
@@ -1,11 +1,19 @@
 package com.greatbit.ip;
 
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IpUtil {
-    public static String IPV4_STR = "";
+    public static String IPV4_STR = "([\\d]{1,3}\\.[\\d]{1,3}\\.[\\d]{1,3}\\.[\\d]{1,3})";
+    public static String KEY_CHAR = ".";
+
+    public static final Pattern IP_PATTERN = Pattern.compile("^" + IPV4_STR + "$");
+    public static final Pattern IP_MASK_PATTERN = Pattern.compile("^" + IPV4_STR + "/([\\d]{1,2})" + "$");
+    public static final Pattern IP_LINE_PATTERN = Pattern.compile("^" + IPV4_STR + "-" + IPV4_STR + "$");
 
     private static ThreadLocal<IpUtil> threadLocal = new ThreadLocal<>();
 
@@ -37,6 +45,15 @@ public class IpUtil {
         return (ips[0] << 24) + (ips[1] << 16) + (ips[2] << 8) + ips[3];//must quote
     }
 
+    public String long2ip(long ip) {
+        StringBuilder ipBuffer = new StringBuilder();
+        for (int i = 3; i >= 0; i--) {
+            ipBuffer.append((ip >> (i * 8)) & 0xFF);
+            if (i != 0) ipBuffer.append(".");
+        }
+        return ipBuffer.toString();
+    }
+
     @Deprecated
     public long ips2long(String ipAddr) {
         String[] ips = ipAddr.split("\\.");
@@ -44,24 +61,76 @@ public class IpUtil {
     }
 
 
-    public Map<Integer, Map<Long, String>> store(String... ipStrs) {
-        Map<Integer, Map<Long, String>> map = new HashMap<>();
+    public void store(Map<Integer, Set<Long>> map, String... ipStrs) {
         Arrays.stream(ipStrs).forEach(t -> {
+            String ip = null;
+            Integer mask = Integer.valueOf(32);
+            Matcher m = IP_PATTERN.matcher(t);
+            if (m.find()) {
+                ip = t;
+            } else {
+                m = IP_MASK_PATTERN.matcher(t);
+                if (m.find()) {
+                    ip = m.group(1);
+                    mask = Integer.valueOf(m.group(2));
 
+                } else {
+                    m = IP_LINE_PATTERN.matcher(t);
+                    if (m.find()) {
+                        ip = m.group(1);
+                        String _ip = m.group(2);
+
+                        mask = netmask(ip, _ip);
+                    }
+                }
+            }
+
+            if (ip != null) {
+                if (!map.containsKey(mask)) map.put(mask, new HashSet<>());
+                map.get(mask).add(prefix(ip, mask));
+            }
         });
-        return map;
+    }
+
+    public long prefix(long ip, int mask) {
+        return ip >> (32 - mask);
+    }
+
+    public long prefix(String ip, int mask) {
+        return prefix(ip2long(ip), mask);
     }
 
     public String key(long ip, int mask) {
         StringBuilder builder = new StringBuilder();
-        builder.append(mask).append(ip);
+        builder.append(mask).append(KEY_CHAR).append(ip >> (32 - mask));
         return builder.toString();
     }
 
     public String key(String ipAddr, int mask) {
         long ip = ip2long(ipAddr);
-        StringBuilder builder = new StringBuilder();
-        builder.append(mask).append(ip >> (32 - mask));
-        return builder.toString();
+        return key(ip, mask);
+    }
+
+    public Integer netmask(String ipStart, String ipEnd) {
+        long v1 = ip2long(ipStart);
+        long v2 = ip2long(ipEnd);
+
+        return netmask(v1, v2);
+    }
+
+    public Integer netmask(Long ipStart, Long ipEnd) {
+        return (32 - Long.bitCount(ipEnd - ipStart));
+    }
+
+    public boolean in(String ipStr, Map<Integer, Set<Long>> map) {
+        long ip = ip2long(ipStr);
+
+        return map.keySet().stream().filter(k -> map.get(k).contains(ip >> (32 - k))).findFirst().isPresent();
+    }
+
+    @Deprecated
+    public boolean in(String ipStr, Set<String> set, Set<Integer> masks) {
+        long ip = ip2long(ipStr);
+        return masks.stream().map(m -> m + "." + (ip >> (32 - m))).filter(k -> set.contains(k)).findFirst().isPresent();
     }
 }
--- java/sqlbuilder/src/test/java/com/example/IpTest.java
+++ java/sqlbuilder/src/test/java/com/example/IpTest.java
@@ -3,28 +3,80 @@ package com.example;
 import com.greatbit.ip.IpUtil;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class IpTest {
     IpUtil ipUtil = IpUtil.open();
 
+    int count = 10000000;
+
     @Test
+    public void testIP() {
+        String ip_s = "192.168.10.0";
+        String ip_e = "192.168.10.255";
+
+        System.out.println("mask:" + ipUtil.netmask(ip_s, ip_e));
+        Map<Integer, Set<Long>> map = new HashMap<>();
+
+        ipUtil.store(map, "192.168.10.0-192.168.10.255", "117.8.0.0-117.15.255.255", "125.39.34.0/23", "61.136.57.0/24");
+        map.forEach((k, v) -> {
+            System.out.println("key:" + k);
+            System.out.println(v);
+            System.out.println("#################################");
+        });
+
+        System.out.println(ipUtil.in("192.168.10.1", map));
+        System.out.println(ipUtil.in("192.168.10.2", map));
+        System.out.println(ipUtil.in("61.136.57.0", map));
+        System.out.println(ipUtil.in("61.136.56.0", map));
+        System.out.println(ipUtil.in("192.168.9.255", map));
+
+
+
+        Set<String> set = new HashSet<>();
+        map.forEach((k, v) -> {
+            v.forEach(vv -> set.add(k + "." + vv));
+        });
+
+        System.out.println("################################");
+        set.forEach(System.out::println);
+        System.out.println("################################");
+        System.out.println(ipUtil.in("192.168.10.1", set,map.keySet()));
+        System.out.println(ipUtil.in("192.168.10.2", set,map.keySet()));
+        System.out.println(ipUtil.in("61.136.57.0", set,map.keySet()));
+        System.out.println(ipUtil.in("61.136.56.0", set,map.keySet()));
+        System.out.println(ipUtil.in("192.168.9.255", set,map.keySet()));
+        System.out.println("################################");
+
+        //time(count, t -> map.containsKey(23));
+        time(count, t -> ipUtil.in("192.168.9.255", map));
+        time(count, t -> ipUtil.in("192.168.9.255", set,map.keySet()));
+    }
+
+    //@Test
     public void testUtil() {
-
-
         System.out.println("255.255.255.255:" + ipUtil.ip2long("255.255.255.255"));
         System.out.println("255.255.255.255:" + ipUtil.ips2long("255.255.255.255"));
 
+        String ipStart_1 = "117.8.0.0";
+        String ipEnd_1 = "117.15.255.255";
+        long v1 = ipUtil.ip2long(ipStart_1);
+        long v2 = ipUtil.ip2long(ipEnd_1);
+        int m1 = netmask(ipStart_1, ipEnd_1);
 
+        System.out.println(ipStart_1 + " ip2long:" + v1 + " long2ip:" + ipUtil.long2ip(v1) + " binary:" + Long.toBinaryString(v1));
+        System.out.println(ipEnd_1 + " ip2long:" + v2 + " long2ip:" + ipUtil.long2ip(v2) + " binary:" + Long.toBinaryString(v2));
+        System.out.println("mask:" + m1 + " " + v1 + " key:" + ipUtil.key(v1, m1));
 
-        netmask("117.8.0.0", "117.15.255.255");
-        netmask("60.24.0.0", "60.27.255.255");
-
-        time(10000000, t -> ipUtil.ip2long("255.255.255.255"));
-        time(10000000, t -> ipUtil.ips2long("255.255.255.255"));
-        time(10000000, t ->  netmask("60.24.0.0", "60.27.255.255"));
+        time(count, t -> ipUtil.long2ip(v1));
+        time(count, t -> ipUtil.ip2long("255.255.255.255"));
+        time(count, t -> ipUtil.ips2long("255.255.255.255"));
+        time(count, t -> netmask("60.24.0.0", "60.27.255.255"));
     }
-
 
     private void time(int count, Consumer t) {
         long start = System.currentTimeMillis();
@@ -37,21 +89,10 @@ public class IpTest {
         System.out.printf("Time use [%d]ms.\n", end - start);
     }
 
-    private void netmask(String ipStart, String ipEnd) {
+    private int netmask(String ipStart, String ipEnd) {
         long v1 = ipUtil.ip2long(ipStart);
         long v2 = ipUtil.ip2long(ipEnd);
 
-        int mask = (32 - Long.bitCount(v2 - v1));
-
-        long key = v1 >> (32 - mask);
-        //ipUtil.key("60.24.3.0", 14);
-
-//        System.out.println(ipStart + ":" + v1 + ":" + Long.toBinaryString(v1));
-//        System.out.println(ipEnd + ":" + v2 + ":" + Long.toBinaryString(v2));
-//        System.out.println(Long.toBinaryString(v2 - v1));
-//
-//        System.out.println("mask:" + mask + " " + key + " key:" + Long.toBinaryString(key));
-//
-//        System.out.println(ipUtil.key("60.24.3.0", 14));
+        return (32 - Long.bitCount(v2 - v1));
     }
 }
